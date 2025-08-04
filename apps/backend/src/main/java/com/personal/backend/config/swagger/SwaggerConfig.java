2c598c929353ba614e354cb7b091d84feb7a8a64
Jang DaeHan <jth0809@naver.com>
2025-08-04 15:41:39 +0900

    [백엔드] 스웨거 prefix 설정 업데이트

---
+++ apps/backend/src/main/java/com/personal/backend/config/swagger/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package com.personal.backend.config.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("/api"));
+    }
+}
