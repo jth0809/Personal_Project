e4c126c3007bbcaaceb141ff5156e3eca0b66c61
Jang DaeHan <jth0809@naver.com>
2025-08-04 16:43:03 +0900

    [백엔드 ,인프라] 잡 오류로 인한 설정 변경

--- apps/backend/src/main/java/com/personal/backend/BackendApplication.java
+++ apps/backend/src/main/java/com/personal/backend/BackendApplication.java
@@ -3,16 +3,9 @@ package com.personal.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
-import com.personal.backend.config.data.Dataproperties;
-import com.personal.backend.config.jwt.JwtProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({
-    Dataproperties.class,
-    JwtProperties.class
-})
+@ConfigurationPropertiesScan
 public class BackendApplication {
 
 	public static void main(String[] args) {
