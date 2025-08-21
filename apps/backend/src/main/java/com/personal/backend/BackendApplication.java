118269fd3f1ca573d087ee63490ec98a9be1399a
Jang DaeHan <jth0809@naver.com>
2025-08-21 21:07:14 +0900

    [백엔드] 아폴로 테스트를 위한 커밋

--- apps/backend/src/main/java/com/personal/backend/BackendApplication.java
+++ apps/backend/src/main/java/com/personal/backend/BackendApplication.java
@@ -10,7 +10,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class BackendApplication {
-	//test
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
